da83e9ba6f0818d3010ebc072743e84e2a9158b8
goj-team <65608717+goj-team@users.noreply.github.com>
2020-05-30 20:18:26 +0800

    Update faq.php

--- faq.php
+++ faq.php
@@ -1,1 +1,41 @@
-不知道些什么呢
+ <!DOCTYPE html>
+  <head>
+    <title>GenesisOJ - 关于我们</title>
+    <?php include "./template/header.php" ?>
+  </head>
+  <body>
+  <?php include "./template/menu.php" ?>
+    <div id="main" style="position:absolute;top:37px;left:5px;width:98%;">
+      <div style="float:left;width:1%;">&nbsp;</div>
+      <div style="float:left;width:99%;">
+        <div id="" style="border: 1px solid gray;">
+  <center>
+    <h3>1.捐赠事宜</h3>
+	<p>本网站为非盈利网站，在您使用本网站期间不收取您的任何费用。<br>为了本网站的可持续性发展，GenesisOJ维护者们需要支付“域名、服务器、题目更新以及安全防护“等费用。<br>
+	如果您可以为我们捐款,我们将衷心感谢您！下面是捐款码，您扫码即可为我们投食。特别提醒：在捐赠信息中一定注明您在本网站的用户名<br>
+	<img src="/feed.jpg"  alt="给我们投食" /><br>
+	如您对捐赠有疑问，请发送邮件至flamesheart@qq.com,谢谢！
+	</p>
+	<h3>2.系统简述</h3>
+	<p> 测评配置:为Hustoj的测评机的二次开发，配置与Hust相同<br>
+		操作系统:Ubuntu 16.04<br></p>
+	<h3>3.网站相关事宜<h3>
+	<h4>3.1 开发成员</h4>
+	<p>主要开发:flamesheart, jyb666, pcoaoy<br>
+	   辅助开发:cbj等
+	</p>
+	<h4>3.2管理人员</h4>
+	<p>flamesheart, jyb666, jiayouba, ytha等</p>
+	<h4>3.3其他事宜</h4>
+	<p>因个人编写水平有限，网站维护、界面设计、网站安全等部分功能开发尚不成熟，如有疑问或建议，请联系通过站长邮箱进行联系<br>
+	站长邮箱:flamesheart@qq.com
+	</p>
+    <p><font size="2">
+      Copyright(C)GenesisOJ 2020 - <?php echo date("Y")?> <br>
+    </font></p>
+  </center>
+        </div>
+      </div>
+    </div>
+  </body>
+ </html>
