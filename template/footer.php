dbd6b498c4e9d78e6814676eec934c5cb94d7ba8
goj-team <65608717+goj-team@users.noreply.github.com>
2020-05-30 21:53:56 +0800

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,5 @@
 <center>
 <p><font size="2" face="arial" color="blue">
-      Copyright(C)GenesisOJ 2020 - <?php echo date("Y")?> <br>
+      Copyright(C)GenesisOJ 2020 - <?php echo date("Y")?>. All Rights Reserved. <br>
  </font></face></color></p>
   </center>
